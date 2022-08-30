96e030b96b423d39a2ebdc18c1d3b7bfefbfb999
xan2301 <2silverghost2@gmail.com>
2022-08-30 14:34:21 +0200

    1.0.0.1

---
+++ src/main/java/Config.java
@@ -0,0 +1,27 @@
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.support.AbstractAnnotationConfigDispatcherServletInitializer;
+
+@Configuration
+@ComponentScan("app")
+@EnableWebMvc
+
+public class Config extends AbstractAnnotationConfigDispatcherServletInitializer {
+
+
+    @Override
+    protected Class<?>[] getRootConfigClasses() {
+        return new Class[0];
+    }
+
+    @Override
+    protected Class<?>[] getServletConfigClasses() {
+        return new Class[]{Config.class};
+    }
+
+    @Override
+    protected String[] getServletMappings() {
+        return new String[]{"/"};
+    }
+}
